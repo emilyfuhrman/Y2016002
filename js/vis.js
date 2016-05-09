52587fcce1841af7a4ec22342f02fae99a21a134
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-05-09 02:37:38 -0400

    initial commit

---
+++ js/vis.js
@@ -0,0 +1,22 @@
+/*window.onload = function(){
+
+	return {
+
+		generate:function(){
+			debugger;
+		}
+	}
+}().generate();*/
+
+var init = function(){
+
+	return {
+
+		generate:function(){
+			debugger;
+		}
+	}
+}
+
+var vis = init();
+vis.generate();
