2a5f17de806b100ad419749c2d8a27b7823b09c7
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-05-09 11:14:04 -0400

    reducing categories to 4

--- js/vis.js
+++ js/vis.js
@@ -13,7 +13,7 @@ window.onload = function(){
 
 		padding_top:45,
 
-		ring_padding:6,
+		ring_padding:5,
 		ring_center:60,
 		
 		getData:function(){
@@ -30,10 +30,21 @@ window.onload = function(){
 			var sum_min = (self.tot_hrs*60) +self.tot_min;
 
 			var svg = d3.select('#vis');
+
 			var rings,
+				cross,
 				nodeG,
+
 				nodesG,
-				nodes;
+				nodes,
+				cross_01,
+				cross_02,
+
+				conns;
+
+			var coord_ctr_x = self.w/2,
+				coord_ctr_y = self.h/2 +self.padding_top -self.ring_center;
+
 			rings = svg.selectAll('circle.ring')
 				.data(d3.range(sum_min));
 			rings.enter().append('circle')
@@ -48,6 +59,30 @@ window.onload = function(){
 					return self.ring_center +i*self.ring_padding;
 				});
 			rings.exit().remove();
+
+			var crossH = 1000;
+
+			cross = svg.selectAll('line.cross')
+				.data([1,2]);
+			cross.enter().append('line')
+				.classed('cross',true);
+			cross
+				.attr('x1',function(d,i){
+					return i === 0 ? coord_ctr_x -(crossH/2) : coord_ctr_x;
+				})
+				.attr('y1',function(d,i){
+					return i === 0 ? coord_ctr_y +self.ring_center : coord_ctr_y -(crossH/2) +self.ring_center;
+				})
+				.attr('x2',function(d,i){
+					return i === 0 ? coord_ctr_x +(crossH/2) : coord_ctr_x;
+				})
+				.attr('y2',function(d,i){
+					return i === 0 ? coord_ctr_y +self.ring_center : coord_ctr_y +(crossH/2) +self.ring_center;
+				});
+			cross.exit().remove();
+
+			var nodeW = 24;
+
 			nodeG = svg.selectAll('g.nodeG')
 				.data([data]);
 			nodeG.enter().append('g')
@@ -58,21 +93,75 @@ window.onload = function(){
 			nodesG.enter().append('g')
 				.classed('nodesG',true);
 			nodesG.exit().remove();
+
 			nodes = nodesG.selectAll('rect.node')
 				.data(function(d){ return [d]; });
 			nodes.enter().append('rect')
 				.classed('node',true);
 			nodes
 				.attr('x',function(d){
-					return self.w/2 -12;
+					return coord_ctr_x -(nodeW/2);
 				})
 				.attr('y',function(d){
-					return self.h/2 +self.padding_top -self.ring_center -(+d.Minutes_Total*self.ring_padding) -12;
+					var dist_from_ctr = (+d.Minutes_Total*self.ring_padding) +(+d.Second/60*self.ring_padding);
+					return coord_ctr_y -(dist_from_ctr) -(nodeW/2);
 				})
-				.attr('width',24)
-				.attr('height',24)
+				.attr('width',nodeW)
+				.attr('height',nodeW)
 				;
 			nodes.exit().remove();
+
+			var crossW = 0.5,
+				crossH = 36;
+
+			cross_01 = nodesG.selectAll('rect.cross_01')
+				.data(function(d){ return [d]; });
+			cross_01.enter().append('rect')
+				.classed('cross_01',true);
+			cross_01
+				.attr('x',function(d){
+					return coord_ctr_x -(crossW/2);
+				})
+				.attr('y',function(d){
+					var dist_from_ctr = (+d.Minutes_Total*self.ring_padding) +(+d.Second/60*self.ring_padding);
+					return coord_ctr_y -(dist_from_ctr) -(crossH/2);
+				})
+				.attr('width',crossW)
+				.attr('height',crossH)
+				;
+			cross_01.exit().remove();
+			cross_02 = nodesG.selectAll('rect.cross_02')
+				.data(function(d){ return [d]; });
+			cross_02.enter().append('rect')
+				.classed('cross_02',true);
+			cross_02
+				.attr('x',function(d){
+					return coord_ctr_x -(crossH/2);
+				})
+				.attr('y',function(d){
+					var dist_from_ctr = (+d.Minutes_Total*self.ring_padding) +(+d.Second/60*self.ring_padding);
+					return coord_ctr_y -(dist_from_ctr) -(crossW/2);
+				})
+				.attr('width',crossH)
+				.attr('height',crossW)
+				;
+			cross_02.exit().remove();
+
+			conns = nodesG.selectAll('line.conn')
+				.data(function(d){ return [d]; });
+			conns.enter().append('line')
+				.classed('conn',true);
+			conns
+				.attr('x1',coord_ctr_x)
+				.attr('y1',coord_ctr_y)
+				.attr('x2',function(d){
+					return coord_ctr_x;
+				})
+				.attr('y2',function(d){
+					var dist_from_ctr = (+d.Minutes_Total*self.ring_padding) +(+d.Second/60*self.ring_padding);
+					return coord_ctr_y -(dist_from_ctr);
+				});
+			conns.exit().remove();
 		}
 	}
 }().getData();
